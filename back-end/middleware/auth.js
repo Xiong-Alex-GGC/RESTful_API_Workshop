ee584d92b9b80ba040bfa3761be03270f146a591
Xiong-Alex <axiong14@ggc.edu>
2024-04-23 23:28:09 -0400

    Revert "init msg"
    This reverts commit 5c815140cf3ff2bff8445652d12fc7811685c09f.

--- back-end/middleware/auth.js
+++
@@ -1,7 +0,0 @@
-// TODO: Implement authentication via Jason Web Token (JWT)
-
-function authenticateToken(req, res, next) {
-    
-}
-
-module.exports = { authenticateToken };
--- back-end/routes/foodRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authenticateToken } = require('../middleware/auth');
-const FoodController = require('../controllers/foodController');
-
-// TODO: Define routes
-
-
-module.exports = router;
